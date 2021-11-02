a338465753b2d80af1d83a8a94f2c3eba43bf79a
zaezins <86808882+zaezins@users.noreply.github.com>
2021-11-02 16:48:16 +0900

    Initial Commit

---
+++ atmcontroller/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  atmcontroller
+//
+//  Created by 이재진 on 2021/11/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
